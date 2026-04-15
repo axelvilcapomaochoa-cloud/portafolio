7b125c0e7d468b71ca64a4b7e47bae9dafc24afd
axelvilcapomaochoa-cloud <axelvilcapomaochoa@gmail.com>
2026-04-15 00:50:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function login() {
-    let user = document.getElementById("usuario").value;
-    let pass = document.getElementById("password").value;
+function login(){
+ let u=document.getElementById('user').value;
+ let p=document.getElementById('pass').value;
 
-    if(user === "admin" && pass === "1234"){
-        window.location = "home.html";
-    } else {
-        document.getElementById("error").innerText = "Datos incorrectos";
-    }
+ if(u==='admin' && p==='1234'){
+   localStorage.setItem("sesion","activa"); // 🔥 GUARDA SESIÓN
+   document.getElementById('login').style.display='none';
+   document.getElementById('contenido').style.display='block';
+ }else{
+   document.getElementById('error').innerText='Datos incorrectos';
+ }
 }
 
+window.onload = function(){
+  if(localStorage.getItem("sesion") === "activa"){
+    document.getElementById('login').style.display='none';
+    document.getElementById('contenido').style.display='block';
+  }
+}
 function logout(){
-    window.location = "index.html";
+  localStorage.removeItem("sesion");
+  location.reload();
 }
